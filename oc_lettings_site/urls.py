d79df09e80003ece083287eb6d2368f98a172b20
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2025-01-28 21:34:49 +0100

    Add 404 and 500 html pages

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from oc_lettings_site.views import index
+from oc_lettings_site import views
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='index'),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls)
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
-
 def index(request):
     return render(request, 'index.html')
+
+def page_not_found(request, exceptions):
+    return render(request, '404.html', status=404)
+
+def server_error(request):
+    return render(request, '500.html', status=500)
