c4fe3a1326d480ab22beaa4f1f493d262602574f
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2025-03-11 13:56:21 +0100

    Update settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,9 +16,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ocappweb.azurewebsites.net']
 
 
 # Application definition
